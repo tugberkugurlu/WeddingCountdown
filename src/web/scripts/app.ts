edb63e94f5f2386472a6feb94d324c543d156b58
tugberkugurlu <tugberkugurlu@gmail.com>
2013-06-12 17:54:31 +0300

    initial commit

---
+++ src/web/scripts/app.ts
@@ -0,0 +1,5 @@
+﻿import model = module('countdownTimer');
+
+var element = document.getElementById('content');
+var countdownTimer = new model.CountdownTimer(7200, element);
+countdownTimer.start();
