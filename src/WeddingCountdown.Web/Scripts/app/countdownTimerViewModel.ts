283a5f6380d8d34c3f0cb0d8835bd94ea9db7331
tugberkugurlu <tugberkugurlu@gmail.com>
2013-06-13 17:57:00 +0300

    installed knockout.TypeScript.DefinitelyTyped package and added countdownTimerViewModel.ts file

---
+++ src/WeddingCountdown.Web/Scripts/app/countdownTimerViewModel.ts
@@ -0,0 +1,3 @@
+// https://github.com/sv01a/TypeScript-Knockoutjs
+// http://stackoverflow.com/questions/12689716/typescript-with-knockoutjs
+// https://groups.google.com/forum/?fromgroups#!topic/knockoutjs/3aUxhzER30U
